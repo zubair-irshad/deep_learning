0f2969e5f9dc1fd5a949b46edac57f376f7f29a4
Muhammad <muhammadzubairirshad@gmail.com>
2020-03-04 15:17:04 -0500

    cnn_visualizations: GradCAM, Guided_BackProp & Style Transfer

--- 2layerNN_and_ConvNet/2_pytorch/run_convnet_tl.sh
+++ 2layerNN_and_ConvNet/2_pytorch/run_convnet_tl.sh
@@ -5,13 +5,10 @@
 #       rate mentioned here to achieve good performance
 #############################################################################
 python -u train_transfer_learning.py \
-    --kernel-size 3 \
-    --hidden-dim 512 \
-    --hidden-dim2 128 \
-    --epochs 50 \
+    --epochs 20 \
     --weight_decay 1e-4 \
     --momentum 0.9 \
-    --batch-size 128 \
+    --batch-size 100 \
     --lr 0.001 | tee convnet.log
 #############################################################################
 #                             END OF YOUR CODE                              #
--- 2layerNN_and_ConvNet/2_pytorch/train_transfer_learning.py
+++ 2layerNN_and_ConvNet/2_pytorch/train_transfer_learning.py
@@ -22,44 +22,33 @@ import torchvision.models as models
 parser = argparse.ArgumentParser(description='CIFAR-10 Example')
 # Hyperparameters
 parser.add_argument('--lr', type=float, metavar='LR',
-                    help='learning rate')
+					help='learning rate')
 parser.add_argument('--momentum', type=float, metavar='M',
-                    help='SGD momentum')
+					help='SGD momentum')
 parser.add_argument('--weight_decay', type=float, default=0.0,
-                    help='Weight decay hyperparameter')
+					help='Weight decay hyperparameter')
 parser.add_argument('--batch-size', type=int, metavar='N',
-                    help='input batch size for training')
+					help='input batch size for training')
 parser.add_argument('--epochs', type=int, metavar='N',
-                    help='number of epochs to train')
-# parser.add_argument('--model',
-#                     choices=['softmax', 'convnet', 'twolayernn', 'mymodel'],
-#                     help='which model to train/evaluate')
-parser.add_argument('--hidden-dim', type=int,
-                    help='number of hidden features/activations')
+					help='number of epochs to train')
 
-parser.add_argument('--hidden-dim2', type=int,
-                    help='number of hidden features/activations')
-parser.add_argument('--hidden-dim3', type=int,
-                    help='number of hidden features/activations')
-parser.add_argument('--kernel-size', type=int,
-                    help='size of convolution kernels/filters')
 # Other configuration
 parser.add_argument('--no-cuda', action='store_true', default=False,
-                    help='disables CUDA training')
+					help='disables CUDA training')
 parser.add_argument('--seed', type=int, default=1, metavar='S',
-                    help='random seed (default: 1)')
+					help='random seed (default: 1)')
 parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
-                    help='input batch size for testing (default: 1000)')
-parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                    help='number of batches between logging train status')
+					help='input batch size for testing (default: 1000)')
+parser.add_argument('--log-interval', type=int, default=20, metavar='N',
+					help='number of batches between logging train status')
 parser.add_argument('--cifar10-dir', default='data',
-                    help='directory that contains cifar-10-batches-py/ '
-                         '(downloaded automatically if necessary)')
+					help='directory that contains cifar-10-batches-py/ '
+						 '(downloaded automatically if necessary)')
 args = parser.parse_args()
 args.cuda = not args.no_cuda and torch.cuda.is_available()
 torch.manual_seed(args.seed)
 if args.cuda:
-    torch.cuda.manual_seed(args.seed)
+	torch.cuda.manual_seed(args.seed)
 
 # Load CIFAR10 using torch data paradigm
 kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
@@ -74,23 +63,23 @@ cifar10_mean_color = [0.49131522, 0.48209435, 0.44646862]
 # std dev of color across training images
 cifar10_std_color = [0.01897398, 0.03039277, 0.03872553]
 transform = transforms.Compose([
-                 transforms.ToTensor(),
-                 transforms.Normalize(cifar10_mean_color, cifar10_std_color),
-            ])
+				 transforms.ToTensor(),
+				 transforms.Normalize(cifar10_mean_color, cifar10_std_color),
+			])
 # Datasets
 train_dataset = CIFAR10(args.cifar10_dir, split='train', download=True,
-                        transform=transform)
+						transform=transform)
 val_dataset = CIFAR10(args.cifar10_dir, split='val', download=True,
-                        transform=transform)
+						transform=transform)
 test_dataset = CIFAR10(args.cifar10_dir, split='test', download=True,
-                        transform=transform)
+						transform=transform)
 # DataLoaders
 train_loader = torch.utils.data.DataLoader(train_dataset,
-                 batch_size=args.batch_size, shuffle=True, **kwargs)
+				 batch_size=args.batch_size, shuffle=True, **kwargs)
 val_loader = torch.utils.data.DataLoader(val_dataset,
-                 batch_size=args.batch_size, shuffle=True, **kwargs)
+				 batch_size=args.batch_size, shuffle=True, **kwargs)
 test_loader = torch.utils.data.DataLoader(test_dataset,
-                 batch_size=args.batch_size, shuffle=True, **kwargs)
+				 batch_size=args.batch_size, shuffle=True, **kwargs)
 
 model = models.resnet18(pretrained=True)
 
@@ -98,27 +87,27 @@ model = models.resnet18(pretrained=True)
 
 # for param in model.parameters():
 #     param.require_grad = False
-    
+	
 from collections import OrderedDict
 
 fc = nn.Sequential(
-             OrderedDict([
-             ('fc1',nn.Linear(512*4,1024)),
-             ('d1', nn.Dropout2d(p=0.25)),
-             ('fc2', nn.Linear(1024,512)),
-             ('d2', nn.Dropout2d(p=0.25)),
-             ('fc3', nn.Linear(512,128)),
-             ('d3', nn.Dropout2d(p=0.25)),
-             ('fc4', nn.Linear(128,10))
-             ]))
+			 OrderedDict([
+			 ('fc1',nn.Linear(512*4,1024)),
+			 ('d1', nn.Dropout2d(p=0.25)),
+			 ('fc2', nn.Linear(1024,512)),
+			 ('d2', nn.Dropout2d(p=0.25)),
+			 ('fc3', nn.Linear(512,128)),
+			 ('d3', nn.Dropout2d(p=0.25)),
+			 ('fc4', nn.Linear(128,10))
+			 ]))
 
 model.fc = fc
 #Define our own classifier
 
 m = nn.Upsample(scale_factor=8)
 
-    
-    
+	
+	
 # Load the model
 # if args.model == 'softmax':
 #     model = models.softmax.Softmax(im_size, n_classes)
@@ -135,7 +124,7 @@ m = nn.Upsample(scale_factor=8)
 # cross-entropy loss function
 criterion = nn.CrossEntropyLoss()
 if args.cuda:
-    model.cuda()
+	model.cuda()
 
 #############################################################################
 # TODO: Initialize an optimizer from the torch.optim package using the
@@ -145,87 +134,84 @@ optimizer = optim.SGD(filter(lambda p: p.requires_grad, model.parameters()), lr=
 # optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
 
 def train(epoch):
-    '''
-    Train the model for one epoch.
-    '''
-    # Some models use slightly different forward passes and train and test
-    # time (e.g., any model with Dropout). This puts the model in train mode
-    # (as opposed to eval mode) so it knows which one to use.
-    model.train()
-    # train loop
-    for batch_idx, batch in enumerate(train_loader):
-        # prepare data
-        images, targets = Variable(batch[0]), Variable(batch[1])
-        images = m(images)
-        if args.cuda:
-            images, targets = images.cuda(), targets.cuda()
-            
-        optimizer.zero_grad()
-        output=model(images)
-        loss = criterion(output,targets)
-        loss.backward()
-        optimizer.step()
-        #############################################################################
-        #                             END OF YOUR CODE                              #
-        #############################################################################
-        # if batch_idx % args.log_interval == 0:
-        #     val_loss, val_acc = evaluate('val', n_batches=4)
-        #     train_loss = loss.data
-        #     examples_this_epoch = batch_idx * len(images)
-        #     epoch_progress = 100. * batch_idx / len(train_loader)
-        #     print('Train Epoch: {} [{}/{} ({:.0f}%)]\t'
-        #           'Train Loss: {:.6f}\tVal Loss: {:.6f}\tVal Acc: {}'.format(
-        #         epoch, examples_this_epoch, len(train_loader.dataset),
-        #         float(epoch_progress), float(train_loss), float(val_loss), float(val_acc)))
+	'''
+	Train the model for one epoch.
+	'''
+	# Some models use slightly different forward passes and train and test
+	# time (e.g., any model with Dropout). This puts the model in train mode
+	# (as opposed to eval mode) so it knows which one to use.
+	model.train()
+	# train loop
+	for batch_idx, batch in enumerate(train_loader):
+		# prepare data
+		images, targets = Variable(batch[0]), Variable(batch[1])
+		images = m(images)
+		if args.cuda:
+			images, targets = images.cuda(), targets.cuda()
+			
+		optimizer.zero_grad()
+		output=model(images)
+		loss = criterion(output,targets)
+		loss.backward()
+		optimizer.step()
+		if batch_idx % args.log_interval == 0:
+			val_loss, val_acc = evaluate('val', n_batches=4)
+			train_loss = loss.data
+			examples_this_epoch = batch_idx * len(images)
+			epoch_progress = 100. * batch_idx / len(train_loader)
+			print('Train Epoch: {} [{}/{} ({:.0f}%)]\t'
+				  'Train Loss: {:.6f}\tVal Loss: {:.6f}\tVal Acc: {}'.format(
+				epoch, examples_this_epoch, len(train_loader.dataset),
+				float(epoch_progress), float(train_loss), float(val_loss), float(val_acc)))
 
-        if batch_idx % args.log_interval == 0:
-            train_loss = loss.data
-            examples_this_epoch = batch_idx * len(images)
-            epoch_progress = 100. * batch_idx / len(train_loader)
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\t'
-                  'Train Loss: {:.6f}\t'.format(
-                epoch, examples_this_epoch, len(train_loader.dataset),
-                float(epoch_progress), float(train_loss)))
+		# if batch_idx % args.log_interval == 0:
+		#     train_loss = loss.data
+		#     examples_this_epoch = batch_idx * len(images)
+		#     epoch_progress = 100. * batch_idx / len(train_loader)
+		#     print('Train Epoch: {} [{}/{} ({:.0f}%)]\t'
+		#           'Train Loss: {:.6f}\t'.format(
+		#         epoch, examples_this_epoch, len(train_loader.dataset),
+		#         float(epoch_progress), float(train_loss)))
 
 def evaluate(split, verbose=False, n_batches=None):
-    '''
-    Compute loss on val or test data.
-    '''
-    model.eval()
-    loss = 0
-    correct = 0
-    n_examples = 0
-    if split == 'val':
-        loader = val_loader
-    elif split == 'test':
-        loader = test_loader
-    for batch_i, batch in enumerate(loader):
-        data, target = batch
-        if args.cuda:
-            data, target = data.cuda(), target.cuda()
-        data, target = Variable(data, volatile=True), Variable(target)
+	'''
+	Compute loss on val or test data.
+	'''
+	model.eval()
+	loss = 0
+	correct = 0
+	n_examples = 0
+	if split == 'val':
+		loader = val_loader
+	elif split == 'test':
+		loader = test_loader
+	for batch_i, batch in enumerate(loader):
+		data, target = batch
+		if args.cuda:
+			data, target = data.cuda(), target.cuda()
+		data, target = Variable(data, volatile=True), Variable(target)
 
-        data = m(data)
-        output = model(data)
-        loss += criterion(output, target).data
-        # predict the argmax of the log-probabilities
-        pred = output.data.max(1, keepdim=True)[1]
-        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
-        n_examples += pred.size(0)
-        if n_batches and (batch_i >= n_batches):
-            break
+		data = m(data)
+		output = model(data)
+		loss += criterion(output, target).data
+		# predict the argmax of the log-probabilities
+		pred = output.data.max(1, keepdim=True)[1]
+		correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+		n_examples += pred.size(0)
+		if n_batches and (batch_i >= n_batches):
+			break
 
-    loss /= n_examples
-    acc = 100. * correct / n_examples
-    if verbose:
-        print('\n{} set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-            split, float(loss), float(correct), float(n_examples), float(acc)))
-    return loss, acc
+	loss /= n_examples
+	acc = 100. * correct / n_examples
+	if verbose:
+		print('\n{} set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+			split, float(loss), float(correct), float(n_examples), float(acc)))
+	return loss, acc
 
 
 # train the model one epoch at a time
 for epoch in range(1, args.epochs + 1):
-    train(epoch)
+	train(epoch)
 
 torch.save(model, 'cifar_resnet.pt')    
 evaluate('test', verbose=True)
